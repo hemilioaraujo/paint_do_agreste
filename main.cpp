3c6faebf0a235ba36115659175cc2f541b98b44b
HLAM <hemilioaraujo@gmail.com>
2021-04-02 15:07:38 -0300

    ajustada a contagem de cliques e mudança da espessura da linha

--- main.cpp
+++ main.cpp
@@ -20,8 +20,12 @@ Aplicação:
 int r=0,g=0,b=0;
 int windowSizeX = 600;
 int windowSizeY = 600;
-int pontos[2][3];
+int pontos[2][2];
 int contador_de_pontos = 0;
+int espessura_da_linha = 0;
+
+// PROTOTIPOS
+void init(void);
 
 // Fun��o callback para desenho
 void desenha(void){
@@ -36,30 +40,29 @@ void desenha(void){
 	//Limpa todos os pixels com a cor de fundo
 	// glClear(GL_COLOR_BUFFER_BIT);
 
-    glColor3ub(r, g, b);
+    // glColor3ub(r, g, b);
 
     glPointSize(5);
+    glLineWidth(espessura_da_linha);
 
-    switch (contador_de_pontos){
+    switch (contador_de_pontos-1){
         case 0:
+            glBegin(GL_POINTS);
+                glColor3ub(r, g, b);
+                glVertex2i(pontos[0][0], pontos[1][0]);
+            glEnd();
             break;
         case 1:
             glBegin(GL_POINTS);
-                glVertex2i(pontos[0][0], pontos[1][0]);
+                glColor3ub(r, g, b);
+                glVertex2i(pontos[0][1], pontos[1][1]);
             glEnd();
-            break;
-        case 2:
-            glBegin(GL_POINTS);
+            glBegin(GL_LINES);
+                glColor3ub(r, g, b);
                 glVertex2i(pontos[0][0], pontos[1][0]);
                 glVertex2i(pontos[0][1], pontos[1][1]);
             glEnd();
-            break;
-        case 3:
-            glBegin(GL_TRIANGLES);
-                glVertex2i(pontos[0][0],pontos[1][0]);
-                glVertex2i(pontos[0][1],pontos[1][1]);
-                glVertex2i(pontos[0][2],pontos[1][2]);
-            glEnd();
+            contador_de_pontos = 0;
             break;
         default:
             break;
@@ -81,6 +84,20 @@ void keyboard(unsigned char key, int x, int y){
         case 27:
             exit(0);
             break;
+        case 'a':
+            init();
+            printf("Limpando tela\n");
+            break;
+        case '+':
+            espessura_da_linha++;
+            printf("Espessura: %d", espessura_da_linha);
+            break;
+        case '-':
+            if (espessura_da_linha > 0){
+                espessura_da_linha--;
+                printf("Espessura: %d", espessura_da_linha);
+            }
+            break;   
         case '1':
             r=255, g=0, b=0;
             break;
@@ -110,7 +127,7 @@ void keyboard(unsigned char key, int x, int y){
             break;
     }
 
-   printf("Cores: RGB (%d,%d,%d)\n",r,g,b);
+//    printf("Cores: RGB (%d,%d,%d)\n",r,g,b);
    glutPostRedisplay();
 }
 
@@ -146,30 +163,27 @@ y = windowSizeY - y;
     if (button == GLUT_LEFT_BUTTON) {
         if (state == GLUT_DOWN) {
             //a��es com o bot�o direito pressionado
-            // [x1][x2][x3]
-            // [y1][y2][y3]
+            // [x1][x2]
+            // [y1][y2]
             switch (contador_de_pontos){
                 case 0:
                     pontos[0][0] = x;
                     pontos[1][0] = y;
+                    // printf("Clique: %d\n", contador_de_pontos);
                     contador_de_pontos++;
                     break;
                 case 1:
                     pontos[0][1] = x;
                     pontos[1][1] = y;
-                    contador_de_pontos++;
-                    break;
-                case 2:
-                    pontos[0][2] = x;
-                    pontos[1][2] = y;
+                    // printf("Clique: %d\n", contador_de_pontos);
                     contador_de_pontos++;
                     break;
                 default:
-                    contador_de_pontos = 0;
                     break;
             }
 
-            printf("x: %d\ny: %d\n\n", x, y);
+            // printf("x: %d\ny: %d\n\n", x, y);
+            
         }
     }
     // glutDisplayFunc(desenha);
@@ -183,6 +197,7 @@ void init (void){
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     //Limpa todos os pixels com a cor de fundo
     glClear(GL_COLOR_BUFFER_BIT);
+    glutPostRedisplay();
 }
 
 //Principal
@@ -192,20 +207,20 @@ int main(int argc, char **argv){
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	//Configura o tamanho da janela
 	glutInitWindowSize (windowSizeX, windowSizeY);
-	//Configura a posi��o inicial da janela
+	//Configura a posição inicial da janela
 	glutInitWindowPosition (100, 100);
 	//Configura a janela
-	glutCreateWindow("Mouse e Teclado no OpenGL");
+	glutCreateWindow("Paint Do Agreste");
 	//configura o teclado
 	glutKeyboardFunc(keyboard);
 	//teclas especiais
 	glutSpecialFunc(specialKeys);
 	//mouse
 	glutMouseFunc(mouse);
-	//Chama a fun��o desenha
+	//Chama a função desenha
 	glutDisplayFunc(desenha);
 	//Inicializa o programa
 	init();
-	//Habilita o loop (m�quina de estados)
+	//Habilita o loop (máquina de estados)
 	glutMainLoop();
 }
